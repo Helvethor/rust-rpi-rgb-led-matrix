d1a420d77b9dff154277783fc21f3d3283f04867
Tyler Holmes <tyler@holmesengineering.com>
2022-01-02 13:05:33 -0800

    first try at getting cross working in GHA CI

--- rpi-led-matrix-sys/build.rs
+++ rpi-led-matrix-sys/build.rs
@@ -14,13 +14,11 @@ fn main() {
     }
 
     // link to c++ std library dylib as the c++ library depends on it
-    println!("cargo:rustc-flags=-l dylib=stdc++");
+    // println!("cargo:rustc-flags=-l dylib=stdc++");
 
     // 0. To guess at if we're on the right platform, look for linux as the system & arm as the architecture
-    // Note I'm checking HOST instead of TARGET since the C++ library depends on natively linking to some libraries
-    //   that are only on rpis
-    let host = std::env::var("HOST").unwrap();
-    if !(host.contains("arm") || host.contains("aarch")) || !host.contains("linux") {
+    let target = std::env::var("TARGET").unwrap();
+    if !(target.contains("arm") || target.contains("aarch")) || !target.contains("linux") {
         eprintln!("rpi-led-matrix-sys detected you're likely not compiling on a raspberry pi");
         std::process::exit(-1);
     }
